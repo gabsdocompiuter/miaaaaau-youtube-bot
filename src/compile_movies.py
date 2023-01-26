ea4a1f56246f6575d88eb219edfd1dcfbef710cd
Gabriel Monteiro <gmonteeeiro@gmail.com>
2023-01-26 09:13:51 -0300

    Create CompileMovies class

---
+++ src/compile_movies.py
@@ -0,0 +1,95 @@
+from moviepy.editor import *
+from glob import glob
+
+import random
+
+
+class CompileMovies:
+    def __init__(self, temp_folder, video_max_duration, compilation_max_duration):
+        self.temp_folder = temp_folder
+        self.video_max_duration = int(video_max_duration)
+        self.compilation_max_duration = int(compilation_max_duration)
+        self.__clips = []
+
+    def compile_videos(self):
+        print()
+        print('starting compilation process...')
+
+        videos_to_add = self.get_videos()
+
+        videos_added = []
+
+        video_duration = 0
+
+        while videos_to_add and video_duration <= self.compilation_max_duration:
+            selecting_video = True
+            video = None
+
+            while selecting_video:
+                video = random.choice(videos_to_add)
+
+                if video.duration > self.video_max_duration:
+                    videos_to_add.remove(video)
+                    continue
+
+                if not videos_added:
+                    selecting_video = False
+                else:
+                    if video.instagram_account != videos_added[-1].instagram_account:
+                        selecting_video = False
+                    elif len(videos_to_add) == 1:
+                        videos_to_add.remove(video)
+
+            print('adding video ' + video.file_name)
+            videos_to_add.remove(video)
+            videos_added.append(video)
+            video_duration += video.duration
+
+            clip = VideoFileClip(video.video_path).resize(height=720)
+            self.__clips.append(clip)
+
+        if not videos_added:
+            return
+
+        print('compiling')
+        compiled_video = concatenate_videoclips(
+            self.__clips, method='compose', padding=0.3)
+        compiled_video.write_videofile('youtube/compiled_video.mp4')
+
+        return videos_added
+
+    def get_videos(self):
+        paths = glob(self.temp_folder + '/*.mp4')
+        videos = []
+
+        for video_path in paths:
+            video_info = self.get_movie_info(video_path)
+            videos.append(video_info)
+
+        return videos
+
+    def get_movie_info(self, video_path):
+        file_name = video_path.split('\\')[-1]
+        instagram_account = file_name.split('---')[0]
+
+        clip = VideoFileClip(video_path)
+
+        movie_info = MovieInfo(instagram_account=instagram_account,
+                               file_name=file_name,
+                               video_path=video_path,
+                               duration=clip.duration,
+                               width=clip.w,
+                               height=clip.h)
+
+        clip.close()
+        return movie_info
+
+
+class MovieInfo:
+    def __init__(self, instagram_account, file_name, video_path, duration, width, height):
+        self.instagram_account = instagram_account
+        self.file_name = file_name
+        self.video_path = video_path
+        self.duration = duration
+        self.widht = width
+        self.height = height
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+from compile_movies import CompileMovies
+from datetime import timedelta
 from dotenv import load_dotenv
+from file_utils import delete_trash_files, delete_temp_files
 from instagram_downloader import InstagramDownloader
-# from file_utils import delete_trash_files
 
 import os
 import file_utils
@@ -10,14 +12,46 @@ def main():
     print('starting bot...')
 
     load_dotenv()
-
     instagram_user = os.environ.get('instagram_username')
     instagram_pass = os.environ.get('instagram_password')
-    temp_folder = 'temp'
+    video_max_duration = os.environ.get('video_max_duration')
+    compilation_max_duration = os.environ.get('compilation_max_duration')
+    temp_folder = os.environ.get('temp_folder')
+
+    delete_temp_files(temp_folder)
 
     scraper = InstagramDownloader(instagram_user, instagram_pass, temp_folder)
     scraper.download_last_memes(days=1)
     file_utils.delete_trash_files(temp_folder)
+
+    compiler = CompileMovies(temp_folder=temp_folder,
+                             video_max_duration=video_max_duration,
+                             compilation_max_duration=compilation_max_duration)
+    videos_added = compiler.compile_videos()
+
+    print(create_description(videos_added))
+
+    # upload to youtube
+
+
+def create_description(videos_added):
+    description = ''
+    time = 0
+
+    for video in videos_added:
+        description_line = get_seconds_in_minutes(
+            time) + ': ' + video.instagram_account + '\n'
+        description += description_line
+        time += video.duration
+
+    return description
+
+
+def get_seconds_in_minutes(seconds):
+    td = str(timedelta(seconds=seconds))
+    td_splited = td.split(':')
+
+    return td_splited[1] + ':' + td_splited[2]
 
 
 if __name__ == '__main__':
